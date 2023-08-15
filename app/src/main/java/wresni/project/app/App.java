b6900ac66df35da2b728f19426d32b685680968b
Wresni Ronggowerdhi <wresni.ronggowerdhi@indivaragroup.com>
2023-08-15 11:44:41 +0700

    Update timezone

--- app/src/main/java/wresni/project/app/App.java
+++ app/src/main/java/wresni/project/app/App.java
@@ -1,11 +1,19 @@
 package wresni.project.app;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class App {
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
+    }
+
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("WIB"));
     }
 }
